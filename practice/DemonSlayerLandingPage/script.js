b6e5f08ca16772f5c241622db0ab689432e655c0
rohitgarwad <rohitgarwad004@gmail.com>
2024-08-12 10:35:34 +0530

    DemonSlayerLandingPage

---
+++ practice/DemonSlayerLandingPage/script.js
@@ -0,0 +1,13 @@
+const btn1 = document.getElementById('add-card-btn-1');
+const btn2 = document.getElementById('add-card-btn-2');
+const btn3 = document.getElementById('add-card-btn-3');
+
+btn1.addEventListener('click', () => {
+    alert('Card Added');
+})
+btn2.addEventListener('click', () => {
+    alert('Card Added');
+})
+btn3.addEventListener('click', () => {
+    alert('Card Added');
+})
